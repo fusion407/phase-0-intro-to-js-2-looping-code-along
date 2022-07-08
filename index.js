68e887021efb2bbc7454dc03e2ac5390a83b942b
fusion407 <fusion407@gmail.com>
2022-07-07 23:56:38 -0500

    commit tree 2041382e2feef309dff12964d8d0fa49de9e64fe

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+function writeCards() {
+    const name = ["Guadalupe", "Ollie", "Aki"];
+    const msg = [];
+    for(let i = 0; i < name.length; i++) {
+        msg[i] = `Thank you, ${name[i]}, for the wonderful surprise gift!`;
+    }
+    return msg;
+}
+function countDown(n) {
+    while(n >= 0) {
+        console.log(n);
+        n--;
+    }
+}
